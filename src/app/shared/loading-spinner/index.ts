122de37c3adcfb475c0917a09bd4fb6205da643e
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2025-08-01 23:48:52 -0500

    feat(ui): export LoadingSpinner from barrel index

---
+++ src/app/shared/loading-spinner/index.ts
@@ -0,0 +1,11 @@
+export { LoadingSpinnerComponent } from './loading-spinner.component';
+export type {
+  LoadingSpinnerConfig,
+  SpinnerSize,
+  SpinnerState,
+  SpinnerMode,
+} from './loading-spinner.component';
+export {
+  LOADING_SPINNER_PRESETS,
+  getSpinnerConfig,
+} from './loading-spinner.config';

16dc7d8c5a3a99f401ed1176f255d1fcf8b484d4
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2025-08-01 21:27:38 -0500

    feat(data): add barrel file for simplified exports

---
+++ src/app/data/index.ts
@@ -0,0 +1,5 @@
+export * from './services/mock-http.service';
+export * from './mock/mock-data';
+export * from './types/mock-http.types';
+export * from './guards/mock-http.guards';
+export * from './utils/mock-http.utils';

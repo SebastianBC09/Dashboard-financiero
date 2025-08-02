ae58710eb44c7cbb6f8e70c0f82467dd53f0b5ab
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2025-08-02 00:23:42 -0500

    feat(ui): export NavigationHeader from barrel index

---
+++ src/app/shared/navigation-header/index.ts
@@ -0,0 +1,12 @@
+export { NavigationHeaderComponent } from './navigation-header.component';
+export type {
+  NavigationItem,
+  UserProfile,
+  NotificationItem,
+} from './navigation-header.component';
+export {
+  DEFAULT_NAVIGATION_ITEMS,
+  DEFAULT_USER_PROFILE,
+  DEFAULT_NOTIFICATIONS,
+  NOTIFICATION_PRESETS,
+} from './navigation-header.config';

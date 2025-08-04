a8252bcbd69c1c4917e4e5e6add464ee53338d9d
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2025-08-04 03:36:13 -0500

    feat(app): configure application routes in AppRoutingModule

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/authentication.guard';
+import { LoanApplicationFormComponent } from './features/loan-request/loan-application-form/loan-application-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./features/authentication/login-page/login-page.component').then(
+        (m) => m.LoginPageComponent,
+      ),
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import(
+        './features/dashboard/dashboard-main/dashboard-main.component'
+      ).then((m) => m.DashboardMainComponent),
+    canActivate: [authGuard],
+  },
+  {
+    path: 'loan-application',
+    component: LoanApplicationFormComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: '**',
+    redirectTo: '/dashboard',
+  },
+];
